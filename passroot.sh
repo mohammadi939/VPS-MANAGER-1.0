ead0dee3cd939394327cc08b08620030e4b5c3af
Ali Mohammadi <48397716+mohammadi939@users.noreply.github.com>
2023-11-07 08:13:38 +0330

    Update and rename senharoot.sh to passroot.sh

--- passroot.sh
+++ passroot.sh
@@ -2,7 +2,7 @@
 clear
 [[ "$(whoami)" != "root" ]] && {
 	clear
-	echo -e "\033[1;31mEXECULTE COMO USUARIO ROOT, \033[1;32m(\033[1;33msudo -i\033[1;32m)\033[0m"
+	echo -e "\033[1;31mRUN AS ROOT USER, \033[1;32m(\033[1;33msudo -i\033[1;32m)\033[0m"
 	exit
 }
 [[ $(grep -c "prohibit-password" /etc/ssh/sshd_config) != '0' ]] && {
@@ -31,10 +31,10 @@ iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8799 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
 iptables -A INPUT -p tcp --dport 1194 -j ACCEPT
-clear && echo -ne "\033[1;32mDEFINA A SENHA ROOT\033[1;37m: "; read senha
+clear && echo -ne "\033[1;32mSET THE ROO PASSWORDT\033[1;37m: "; read senha
 [[ -z "$senha" ]] && {
-echo -e "\n\033[1;31mSENHA INVALIDA !\033[0m"
+echo -e "\n\033[1;31mINVALID PASSWORD !\033[0m"
 exit 0
 }
 echo "root:$senha" | chpasswd
-echo -e "\n\033[1;31m[ \033[1;33mOK ! \033[1;31m]\033[1;37m - \033[1;32mSENHA DEFINIDA ! \033[0m"
+echo -e "\n\033[1;31m[ \033[1;33mOK ! \033[1;31m]\033[1;37m - \033[1;32mPASSWORD SET ! \033[0m"
